c23e0d4b3e95500c2ba3e36e0a91a1b3cde46114
election-thai <election.code.th@gmail.com>
2024-10-04 17:12:09 +0700

    Update t-script.js

--- t-script.js
+++ t-script.js
@@ -3,6 +3,6 @@ const teacherData = JSON.parse(localStorage.getItem('loggedInTeacher'));
 
 if (teacherData) {
     // แสดงข้อมูลครู
-    document.getElementById('p-name').textContent = `${teacherData.firstName} ${teacherData.lastName}`;
-    document.getElementById('p-photo').src = teacherData.profilePic || 'default-photo.jpg';
+    document.getElementById('teacher-name').textContent = `${teacherData.firstName} ${teacherData.lastName}`;
+    document.getElementById('teacher-photo').src = teacherData.profilePic || 'default-photo.jpg';
 }
