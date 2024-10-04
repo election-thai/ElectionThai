cffb550c60174592fb0cb97244ac8401eb329981
election-thai <election.code.th@gmail.com>
2024-10-04 16:38:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@300;400;500;600;700&family=Kanit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Login - ครูโรงเรียนสาธิตมหาวิทยาลัยขอนแก่น</title>
-    <link rel="stylesheet" href="style.css">
     <style>
         body {
             margin: 0;
