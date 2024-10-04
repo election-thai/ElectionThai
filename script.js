e0c7693638ec8a309e67e31eeaa0f331e08f4454
election-thai <election.code.th@gmail.com>
2024-10-04 16:42:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+async function login(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
+    try {
+        const response = await fetch("people.json");
+        const teachers = await response.json();
+
+        const teacher = teachers.find(t => t.username === username && t.password === password);
+
+        if (teacher) {
+            sessionStorage.setItem("loggedInUser", JSON.stringify(teacher));
+            window.location.href = "teacher.html";  // เปลี่ยนไปยังหน้า teacher.html
+        } else {
+            document.getElementById("error").textContent = "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
+        }
+    } catch (error) {
+        console.error("เกิดข้อผิดพลาดในการดึงข้อมูล", error);
+    }
+}
